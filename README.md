314214572f019b1e690e8d4922eee838cdc2abcf
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2025-12-08 09:45:41 +0000

    Setup my little webpage template

---
+++ README.md
@@ -0,0 +1,5 @@
+# HomeCord's Website
+
+This is the source code for HomeCord's website.
+
+You can find the live site at [homecord.github.io](https://homecord.github.io) once this Buildathon project has reached stability needed to be published.
---
+++ components/nav-bar.jsx
@@ -0,0 +1,11 @@
+import './nav-bar.css'
+
+export default function NavigationBar() {
+    return (
+        <nav className='nav-bar'>
+            <ul>
+                <li><a href='../index.html'>Home</a></li>
+            </ul>
+        </nav>
+    );
+}
