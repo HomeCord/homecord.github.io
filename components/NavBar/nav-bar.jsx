113bc5931bdf70c627a986b7c17e18de7f8564ec
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2026-01-09 09:51:26 +0000

    Update nav-bar.jsx

--- components/NavBar/nav-bar.jsx
+++ components/NavBar/nav-bar.jsx
@@ -4,7 +4,7 @@ export default function NavigationBar() {
     return (
         <nav className='nav-bar'>
             <ul>
-                <li><a href='../index.html'>HomeCord</a></li>
+                <li><a href='../'>HomeCord</a></li>
             </ul>
         </nav>
     );
